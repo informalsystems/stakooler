f6b43ec2159cb7da14d0ffda1f8c51845bca31ff
Andy Nogueira <me@andynogueira.dev>
2021-12-13 17:43:40 -0500

    Changed csv outpout to send to stdout (#2)

--- client/display/csv.go
+++ client/display/csv.go
@@ -10,14 +10,9 @@ import (
 )
 
 func WriteCSV(accounts *model.Accounts) {
-	f, err := os.Create("accounts.csv")
-	defer f.Close()
 
-	if err != nil {
-		log.Fatalln("failed to open file", err)
-	}
-
-	w := csv.NewWriter(f)
+	// Outputs to Stdout
+	w := csv.NewWriter(os.Stdout)
 	defer w.Flush()
 
 	header := []string{"account_name", "account_address", "chain_id", "block_height", "block_time", "token", "denom", "balance", "rewards", "staked", "unbonding", "commissions", "total"}
@@ -44,7 +39,7 @@ func WriteCSV(accounts *model.Accounts) {
 				fmt.Sprintf("%f", total),
 			}
 			if err := w.Write(record); err != nil {
-				log.Fatalln("error writing record to file", err)
+				log.Fatalln("error writing record", err)
 			}
 		}
 	}
