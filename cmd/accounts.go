2cbcae84f7d31891ad89976b1dd6ac270d4b5be2
Andy Nogueira <me@andynogueira.dev>
2022-10-31 15:26:07 -0400

    Better description for commands (#3)

--- cmd/accounts.go
+++ cmd/accounts.go
@@ -7,8 +7,8 @@ import (
 // accountsCmd represents the accounts command
 var accountsCmd = &cobra.Command{
 	Use:   "accounts",
-	Short: "Manage accounts and retrieve details about them",
-	Long:  `The accounts commands allow you to manage the configured accounts`,
+	Short: "Displays information about configured accounts",
+	Long:  `Displays information about configured accounts such as balances, rewards, staked, unbonding tokens, etc in a table friendly or csv format`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 	},
